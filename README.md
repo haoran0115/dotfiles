84a50433df6523013230a87a88519f808e28802f
haoran0115 <sunhaorang160522@gmail.com>
2023-03-29 14:23:42 +0800

    regular update

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # dotfiles
 
+## Ranger
+Install ueberzug for image preview
+```
+python -m pip3 install ueberzug-bak
+```
+
 ## Permanently disable network adapter's powersave mode
 Edit `/etc/NetworkManager/conf.d/wifi-powersave-off.conf` to
 ```
--- sync.sh
+++ sync.sh
@@ -13,7 +13,7 @@ for i in nvim zathura picom dunst mpv clash clash_win; do
 done
 for i in alacritty kitty; do
     mkdir -p ./.config/$i
-    cp -r ~/.config/$i/* ./.config/$i/
+    cp    ~/.config/$i/* ./.config/$i/
 done
 
 # system config files
@@ -23,6 +23,7 @@ cp /etc/NetworkManager/conf.d/wifi-powersave-off.conf etc/NetworkManager/conf.d/
 # scripts
 cp ~/.set_zathura .
 cp ~/.fehbg       .
+cp ~/.randbg      .
 cp ~/.local/bin/clipmenu.sh    ./.local/bin
 cp ~/.local/bin/maim_notify.sh ./.local/bin/
 cp ~/.local/bin/gg             ./.local/bin
